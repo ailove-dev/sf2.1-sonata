eb5106f892ddba04c994e6eb3968311ecdd53f2a
JohnPoison <truefiresnake@gmail.com>
2012-12-04 15:43:49 +0400

    moved ok php sdk to vendors

--- src/Ailove/OKBundle/DependencyInjection/OKExtension.php
+++ src/Ailove/OKBundle/DependencyInjection/OKExtension.php
@@ -30,7 +30,7 @@ class OKExtension extends Extension
         $container->setParameter('ok.redirect_uri', $config['redirect_uri']);
         $container->setParameter('ok.dialog_url', $config['dialog_url']);
         $container->setParameter('ok.oauth_proxy_class', $config['oauth_proxy_class']);
-        $container->setParameter('ok.sdk_file', $config['sdk_file']);
+//        $container->setParameter('ok.sdk_file', $config['sdk_file']);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
